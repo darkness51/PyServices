26f4d4fa7479f997f899091320e84b5665770722
Carlos Aguilar <caguilar@dwdandsolutions.com>
2011-02-09 01:00:38 -0600

    Agregado el programa que crear el archivo desktop y la correspondiente plantilla del archivo

---
+++ src/create_desktop_file.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os, re
+
+def main():
+    contenido = ''
+    
+    try:
+        f = open("../template.txt", 'r')
+        d = open("../PyServices.desktop", 'w+')
+    except IOError:
+        print "Error al leer el fichero"
+        
+    for line in f:
+        r = re.search('Exec', line)
+        if r:
+            contenido += "Exec=" + os.getcwd() + '/MainApp.py\n'
+        else:
+            contenido += line
+    
+    d.write(contenido)
+    f.close()
+    d.close()    
+    
+if __name__ == "__main__":
+    main()
+    
